0da9cfec8839eb914aca4511b4bdd8046e76f33c
El-even-11 <zhao_ziqian@sjtu.edu.cn>
2022-03-20 00:49:38 -0700

    prj1: client framework

---
+++ src/project1/problem4/client.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    char buffer[256];
+    portno = 2050;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        printf("ERROR opening socket\n");
+        exit(1);
+    }
+    server = gethostbyname("127.0.0.1");
+    if (server == NULL)
+    {
+        printf("ERROR no such host\n");
+        exit(0);
+    }
+    bzero((char *)&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(portno);
+
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("ERROR connecting\n");
+        exit(1);
+    }
+    printf("Please enter the message:\n");
+
+    return 0;
+}
