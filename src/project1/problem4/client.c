ba20e178b6af6c893eaf240f069fba3aa7bb1f8c
El-even-11 <zhao_ziqian@sjtu.edu.cn>
2022-03-20 05:38:26 -0700

    prj1: server & client basically done

--- src/project1/problem4/client.c
+++ src/project1/problem4/client.c
@@ -7,36 +7,55 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
+#define BUFFER_SIZE 256
+#define PORTNO 2050
+
 int main(int argc, char *argv[])
 {
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-    char buffer[256];
-    portno = 2050;
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
     {
         printf("ERROR opening socket\n");
         exit(1);
     }
+
+    struct hostent *server;
     server = gethostbyname("127.0.0.1");
     if (server == NULL)
     {
         printf("ERROR no such host\n");
         exit(0);
     }
+
+    struct sockaddr_in serv_addr;
     bzero((char *)&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-    serv_addr.sin_port = htons(portno);
+    serv_addr.sin_port = htons(PORTNO);
 
     if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
         printf("ERROR connecting\n");
         exit(1);
     }
-    printf("Please enter the message:\n");
+
+    char buffer[BUFFER_SIZE];
+    while (1)
+    {
+        printf("Please enter the message:\n");
+        bzero(buffer, BUFFER_SIZE);
+        gets(buffer, BUFFER_SIZE - 1, stdin);
+        // printf("len : %d\n", strlen(buffer));
+        write(sockfd, buffer, strlen(buffer));
+        if (strcmp(buffer, ":q") == 0)
+        {
+            printf("Client closing ...\n");
+            return 0;
+        }
+        bzero(buffer, BUFFER_SIZE);
+        read(sockfd, buffer, BUFFER_SIZE - 1);
+        printf("From server: %s\n", buffer);
+    }
 
     return 0;
 }
--- src/project1/problem4/server.c
+++ src/project1/problem4/server.c
@@ -6,39 +6,129 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <pthread.h>
 
-void *serve(void *newsockfd);
+#define PORTNO 2050
+#define BUFFER_SIZE 256
+
+pthread_mutex_t mu = PTHREAD_MUTEX_INITIALIZER;
+int client_cnt = 0;
+
+void encrypt(char buf[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        if (buf[i] >= 'a' && buf[i] <= 'z')
+        {
+            int offset = (buf[i] - 'a' + 3) % 26;
+            buf[i] = (char)('a' + offset);
+        }
+        if (buf[i] >= 'A' && buf[i] <= 'Z')
+        {
+            int offset = (buf[i] - 'A' + 3) % 26;
+            buf[i] = (char)('A' + offset);
+        }
+    }
+}
+
+void *serve(void *sockfd)
+{
+    pthread_detach(pthread_self());
+    int available = 0;
+    int newsockfd = (int)(*((int *)sockfd));
+    char buffer[BUFFER_SIZE];
+
+    while (!available)
+    {
+        bzero(buffer, BUFFER_SIZE);
+        int n = read(newsockfd, buffer, BUFFER_SIZE - 1);
+        if (strcmp(buffer, ":q") == 0)
+        {
+            close(newsockfd);
+            return;
+        }
+        pthread_mutex_lock(&mu);
+        if (client_cnt < 2)
+        {
+            client_cnt++;
+            available = 1;
+        }
+        pthread_mutex_unlock(&mu);
+
+        if (available)
+        {
+            encrypt(buffer, n);
+            write(newsockfd, buffer, n);
+            break;
+        }
+        write(newsockfd, "Please wait ...", 15);
+    }
+
+    while (available)
+    {
+        bzero(buffer, BUFFER_SIZE);
+        int n = read(newsockfd, buffer, BUFFER_SIZE - 1);
+        if (strcmp(buffer, ":q") == 0)
+        {
+            break;
+        }
+        encrypt(buffer, n);
+        write(newsockfd, buffer, n);
+    }
+
+    pthread_mutex_lock(&mu);
+    client_cnt--;
+    pthread_mutex_unlock(&mu);
+    close(newsockfd);
+}
 
 int main(int argc, char *argv[])
 {
-    int sockfd, newsockfd, portno, clilen, n;
-    char buffer[256];
-    struct sockaddr_in serv_addr, cli_addr;
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
     {
         printf("ERROR opening socket\n");
         exit(1);
     }
+
+    struct sockaddr_in serv_addr;
     bzero((char *)&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    portno = 2050;
-    serv_addr.sin_port = htons(portno);
+    serv_addr.sin_port = htons(PORTNO);
     serv_addr.sin_addr.s_addr = INADDR_ANY;
+
     if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
         printf("ERROR on binding\n");
         exit(1);
     }
     listen(sockfd, 5);
-    clilen = sizeof(cli_addr);
+
     printf("Server initialing...\n");
 
-    return 0;
-}
+    struct sockaddr_in cli_addr;
+    int clilen = sizeof(cli_addr);
+    int newsockfd;
+    int ret;
 
-void *serve(void *sockfd)
-{
-    int newsockfd = (int)(*((int *)sockfd));
-    
+    while (1)
+    {
+        newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+        if (newsockfd < 0)
+        {
+            printf("ERROR on accepting\n");
+            exit(1);
+        }
+
+        pthread_t thread;
+        ret = pthread_create(&thread, NULL, serve, (void *)&newsockfd);
+        if (ret)
+        {
+            printf("ERROR on creat thread\n");
+            exit(1);
+        }
+    }
+
+    return 0;
 }
